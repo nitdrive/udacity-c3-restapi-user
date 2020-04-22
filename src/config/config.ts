20852baf70b04ef50061090f27975a4cf9497cf6
Nithin <nithin.ayiluri@gmail.com>
2020-04-21 22:22:44 -0500

    first commit

---
+++ src/config/config.ts
@@ -0,0 +1,26 @@
+export const config = {
+  "dev": {
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": "postgres",
+    "aws_reigion": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_BUCKET
+  },
+  "prod": {
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": "postgres",
+    "aws_reigion": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_BUCKET
+  },
+  "jwt": {
+    "secret": process.env.JWT_SECRET
+  }
+
+}
